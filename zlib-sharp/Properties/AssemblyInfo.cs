2df610c954774232984b6618833ccccf3188f28e
Admiral H. Curtiss <pikachu025@gmail.com>
2021-03-22 00:00:14 +0100

    Set proper copyright/version in AssemblyInfo.

--- zlib-sharp/Properties/AssemblyInfo.cs
+++ zlib-sharp/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("zlib-sharp")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("zlib © 1995-2017 Jean-loup Gailly and Mark Adler; zlib-sharp © 2021 Admiral H. Curtiss")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.2.11")]
